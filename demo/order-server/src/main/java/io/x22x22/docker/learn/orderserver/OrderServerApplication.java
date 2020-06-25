9c6ee3ad6142021cab11b526d2f5982bafbf9bf0
x22x22 <rootshellexp@gmail.com>
2020-06-25 13:26:29 +0800

    init
    init

---
+++ demo/order-server/src/main/java/io/x22x22/docker/learn/orderserver/OrderServerApplication.java
@@ -0,0 +1,13 @@
+package io.x22x22.docker.learn.orderserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderServerApplication.class, args);
+	}
+
+}
---
+++ demo/order-server/src/test/java/io/x22x22/docker/learn/orderserver/OrderServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.x22x22.docker.learn.orderserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
