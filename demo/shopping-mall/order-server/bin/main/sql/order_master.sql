7b45231b3bf63be32907b45c470509e433266415
x22x22 <rootshellexp@gmail.com>
2020-07-02 21:11:14 +0800

    场景1
    场景1

--- demo/shopping-mall/order-server/bin/main/sql/order_master.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO order_master (goods_code, goods_name, amount, created_at, updated_at)
-VALUES ('car_mazda_01', '马自达01', 1000000, '2020-06-26 19:12:03', '2020-06-26 20:12:07')
--- demo/shopping-mall/order-server/bin/main/sql/schema.sql
+++
@@ -1,1 +0,0 @@
-CREATE TABLE order_master (id SERIAL PRIMARY KEY, goods_code VARCHAR(255), goods_name VARCHAR(255), amount decimal , created_at datetime , updated_at datetime );
